cc0d7f320a9d229eb7c90f2a7731c4407bafad34
alygacutan <44958734+alygacutan@users.noreply.github.com>
2018-11-26 00:13:15 +0800

    Update HomeV2.php

--- user/HomeV2.php
+++ user/HomeV2.php
@@ -1,5 +1,7 @@
 <?php
-	include_once 'db.php';
+	include_once 'db.php'; 
+	$_SESSION['genre_name']="";
+	$_SESSION['searchkey'] ="";
 ?>
 
 <!DOCTYPE html>
@@ -12,29 +14,61 @@
 <title>MyAnimeLite</title>
 <!-----------------------THE HEAD ------------------------>
 <header>
-	<div class="searchbar">
+	<div class="searchbar"><form action="searchpage.php" method="post">
 		<button type="submit"><i class="fa fa-search"></i></button>
-		<input type="text" placeholder="Search...">
-		<p class="logo">MyAnimeLite</p>
+		<input type="text" placeholder="Search..." name="input" value="">
+	
+		<p class="logo">MyAnimeLite</p></form>
 	</div>
 </header>
 </head>
 
 <div class="sidenav">
 	<h2>MyAnimeLite</h2>
-	<a href=""> Link</a>
+	<a href="HomeV2.php"> Home</a>
 	<a> Link</a>
 	<a> Link</a>
-	<!--<div class="dropdown">-->
 		<a> genres: </a>
 		<div class="dropdown-content">
-			<a>Action</a><a>Adventure</a><a>Cars</a><a>Comedy</a><a>Dementia</a><a>Demons</a><a>Drama</a><a>Fantasy</a><a>Game</a>
-			<a>Historical</a><a>Horror</a><a>Josei</a><a>Kids</a><a>Magic</a><a>Martial Arts</a><a>Mecha</a><a>Military</a>
-			<a>Music</a><a>Mystery</a><a>Parody</a><a>Police</a><a>Psychological</a><a>Romance</a><a>Samurai</a><a>School</a>
-			<a>Sci-Fi</a><a>Seinen</a><a>Shoujo</a><a>Shoujo Ai</a><a>Shounen</a><a>Shounen Ai</a><a>Slice of Life</a>
-			<a>Space</a><a>Sports</a><a>Super Power</a><a>Supernatural</a><a>Thriller</a><a>Vampire</a>
+			<a href="CategoryPage.php?link=Action">Action</a>
+			<a href="CategoryPage.php?link=Adventure">Adventure</a>
+			<a href="CategoryPage.php?link=Cars">Cars</a>
+			<a href="CategoryPage.php?link=Comedy">Comedy</a>
+			<a href="CategoryPage.php?link=Dementia">Dementia</a>
+			<a href="CategoryPage.php?link=Demons">Demons</a>
+			<a href="CategoryPage.php?link=Drama">Drama</a>
+			<a href="CategoryPage.php?link=Fantasy">Fantasy</a>
+			<a href="CategoryPage.php?link=Game">Game</a>
+			<a href="CategoryPage.php?link=Historical">Historical</a>
+			<a href="CategoryPage.php?link=Horror">Horror</a>
+			<a href="CategoryPage.php?link=Josei">Josei</a>
+			<a href="CategoryPage.php?link=Kids">Kids</a>
+			<a href="CategoryPage.php?link=Magic">Magic</a>
+			<a href="CategoryPage.php?link=Martial">Martial Arts</a>
+			<a href="CategoryPage.php?link=Mecha">Mecha</a>
+			<a href="CategoryPage.php?link=Military">Military</a>
+			<a href="CategoryPage.php?link=Music">Music</a>
+			<a href="CategoryPage.php?link=Mystery">Mystery</a>
+			<a href="CategoryPage.php?link=Parody">Parody</a>
+			<a href="CategoryPage.php?link=Police">Police</a>
+			<a href="CategoryPage.php?link=Psychological">Psychological</a>
+			<a href="CategoryPage.php?link=Romance">Romance</a>
+			<a href="CategoryPage.php?link=Samurai">Samurai</a>
+			<a href="CategoryPage.php?link=School">School</a>
+			<a href="CategoryPage.php?link=Sci-Fi">Sci-Fi</a>
+			<a href="CategoryPage.php?link=Seinen">Seinen</a>
+			<a href="CategoryPage.php?link=Shoujo">Shoujo</a>
+			<a href="CategoryPage.php?link=Shoujo Ai">Shoujo Ai</a>
+			<a href="CategoryPage.php?link=Shounen">Shounen</a>
+			<a href="CategoryPage.php?link=Shounen Ai">Shounen Ai</a>
+			<a href="CategoryPage.php?link=Slice of Life">Slice of Life</a>
+			<a href="CategoryPage.php?link=Space">Space</a>
+			<a href="CategoryPage.php?link=Sports">Sports</a>
+			<a href="CategoryPage.php?link=Super Power">Super Power</a>
+			<a href="CategoryPage.php?link=Supernatural">Supernatural</a>
+			<a href="CategoryPage.php?link=Thriller">Thriller</a>
+			<a href="CategoryPage.php?link=Vampire">Vampire</a>
 		</div>
-	<!--</div>-->
 
 </div>
 <!-----------------------THE BODY ------------------------>
@@ -48,17 +82,35 @@
 			<img class="preview_img" src="http://originalneoglide.com/wp-content/uploads/2018/05/hd-poster-death-note-anime-death-note-1.jpg">
 		</div><br><br>
 	</div>
-	<p id="title"> Trending Anime</p>
+	<p id="title"> Most Popular</p>
 		<div class="gallery">
 			<?php 
-				$sql="SELECT poster_link FROM Anime ORDER BY score DESC LIMIT 15";
+				$sql="SELECT poster_link, title_eng FROM Anime ORDER BY rank_popularity ASC LIMIT 14";
 				$result=mysqli_query($connection,$sql);
 
 				if ($result->num_rows > 0) {
 			    // output data of each row
 				    while($row = $result->fetch_assoc()) {
-				    	if()
-				        echo "<img class='gallery' src=". $row["poster_link"].">";
+				    	if($row['title_eng']!=NULL)
+				    	echo "<div class='img-title'><img class='gallery' src=". $row["poster_link"]."><br class='space'><p class='title'>". $row["title_eng"]."</p></div>";
+				    }
+				} else {
+				    echo "0 results";
+				}
+			?>
+		</div>
+		<br><br>
+	<p id="title"> Top Rated</p> 
+		<div class="gallery">
+			<?php 
+				$sql="SELECT poster_link, title_eng FROM Anime ORDER BY score DESC LIMIT 14";
+				$result=mysqli_query($connection,$sql);
+
+				if ($result->num_rows > 0) {
+			    // output data of each row
+				    while($row = $result->fetch_assoc()) {
+				    	if($row['title_eng']!=NULL)
+				        echo "<div class='img-title'><img class='gallery' onerror='this.style.display = 'none' src=". $row["poster_link"]."><br><p class='title'>". $row["title_eng"]."</p></div>";
 				    }
 				} else {
 				    echo "0 results";
@@ -66,28 +118,23 @@
 			?>
 		</div>
 	<br><br>
-	<p id="title"> Top Airing</p>
+	<p id="title"> Best of 2018</p>
 		<div class="gallery">
-			<img class="gallery" src="https://images-na.ssl-images-amazon.com/images/I/91kjVOEopVL._SY606_.jpg">
-			<img class="gallery" src="http://assets-cf.gbeye.com/thumbnails/full_size_204762_1500563995.jpg">
-			<img class="gallery" src="https://vignette.wikia.nocookie.net/trinity-seven/images/b/b2/Trinity_Seven_Anime_Poster.jpg">
-			<img class="gallery" src="https://upload.wikimedia.org/wikipedia/ru/4/41/Orange_anime.jpg">
-			<img class="gallery" src="https://images-na.ssl-images-amazon.com/images/I/61GI-8ZL4uL.jpg">
-			<img class="gallery" src="http://www.prologue.ca/DATA/LIVRE/grande/9782505072140~v~Boruto__Naruto_next_generations__Agenda_2018-2019.jpg">
-			<img class="gallery" src="https://images-na.ssl-images-amazon.com/images/I/91kjVOEopVL._SY606_.jpg">
-			<img class="gallery" src="https://vignette.wikia.nocookie.net/dragonball/images/0/00/Dragon_Ball_Super_Poster.jpg">
-		</div>
-	<br><br>
-	<p id="title"> All-time Top</p>
-		<div class="gallery">
-			<img class="gallery" src="https://images-na.ssl-images-amazon.com/images/I/61GI-8ZL4uL.jpg">
-			<img class="gallery" src="https://vignette.wikia.nocookie.net/trinity-seven/images/b/b2/Trinity_Seven_Anime_Poster.jpg">
-			<img class="gallery" src="https://images-na.ssl-images-amazon.com/images/I/91kjVOEopVL._SY606_.jpg">
-			<img class="gallery" src="https://upload.wikimedia.org/wikipedia/ru/4/41/Orange_anime.jpg">
-			<img class="gallery" src="https://images-na.ssl-images-amazon.com/images/I/91kjVOEopVL._SY606_.jpg">
-			<img class="gallery" src="http://assets-cf.gbeye.com/thumbnails/full_size_204762_1500563995.jpg">
-			<img class="gallery" src="https://vignette.wikia.nocookie.net/dragonball/images/0/00/Dragon_Ball_Super_Poster.jpg">
-			<img class="gallery" src="http://www.prologue.ca/DATA/LIVRE/grande/9782505072140~v~Boruto__Naruto_next_generations__Agenda_2018-2019.jpg">
+			
+			<?php 
+				$sql="SELECT poster_link, title_eng FROM Anime, Aired WHERE year=2018 AND season='Winter' AND Aired.anime_id = Anime.anime_id ORDER BY Anime.rank_popularity ASC LIMIT 14";
+				$result=mysqli_query($connection,$sql);
+
+				if ($result->num_rows > 0) {
+			    // output data of each row
+				    while($row = $result->fetch_assoc()) {
+				    	if($row['title_eng']!=NULL)
+				        echo "<div class='img-title'><img class='gallery' src=". $row["poster_link"]."><br class='space'><p class='title'>". $row["title_eng"]."</p></div>";
+				    }
+				} else {
+				    echo "0 results";
+				}
+			?>
 		</div>
 </center>
 <footer></footer>
@@ -95,7 +142,15 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script>
-
+(function() {
+    var allimgs = document.images;
+    var y = document.getElementsByClassName("img-title");
+    for (var i = 0; i < y.length; i++) {
+        allimgs[i].onerror = function() {
+            //this.style.display = "none"; // Other elements aren't affected.
+        }
+    }
+})();
 var slideIndex = 0;
 carousel();
 
@@ -110,9 +165,14 @@ function carousel() {
     x[slideIndex-1].style.display = "block"; 
     setTimeout(carousel, 5000); 
 }
+
+function search(){
+	var input = document.getElementById("search");
+}
 </script>
 </html>
 
 <?php
+
 $connection->close();
 ?>
