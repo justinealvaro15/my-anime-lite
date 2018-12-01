cd7af07eb0ed38fd363685eeb80aed412c7b30a8
justinealvaro15 <jpalvaro@up.edu.ph>
2018-12-01 16:15:51 +0800

    Added login and registration

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MyAnimeLite</title>
+	<link rel="stylesheet" type="text/css" href="stylev2.css">
+</head>
+<body>
+	<h1>MyAnimeList</h1>	
+	<form class="container" method="post" action="login.php">
+
+		<?php include('errors.php'); ?>
+		<div class="input-group">
+			<h2>Log In</h2>
+		</div>
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user" id="login">Login</button>
+			<script>
+				var btn = document.getElementById('login');
+				btn.addEventListener('click', function() {
+					document.location.href = '<?php echo "user/HomeV2.php"; ?>';
+				});
+			</script>
+		</div>
+		<p>
+			Create an account! <a href="register.php" style="color: #f99a2c">Sign up</a>
+		</p>
+	</form>
+
+
+</body>
+</html>
