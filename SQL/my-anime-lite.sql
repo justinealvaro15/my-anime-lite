91843fd4774c6fae2f4f0769ce7bd8116812c688
justinealvaro15 <jpalvaro@up.edu.ph>
2018-12-03 01:07:44 +0800

    Updated sample data for Users table

--- SQL/my-anime-lite.sql
+++ SQL/my-anime-lite.sql
@@ -22981,7 +22981,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(1, 'dabaseislife1', 'cs165WVW@up.edu.ph', '416e6987c66e47032a0bce171bb269f2'),
+(1, 'databaseislife1', 'cs165WVW@up.edu.ph', '416e6987c66e47032a0bce171bb269f2'),
 (2, 'jmendoza1', 'jmendoza@cs165.com', '416e6987c66e47032a0bce171bb269f2');
 
 --
